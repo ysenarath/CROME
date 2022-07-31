280839974a90708d0173f72e9e891519a921cd31
Yasas <ysenarath.93@gmail.com>
2022-07-31 10:34:48 -0400

    Add view to package

--- crome/dashboard/views/__init__.py
+++ crome/dashboard/views/__init__.py
@@ -1,5 +1,6 @@
 from crome.dashboard.views.explore_dataset_view import explore_dataset_view
 from crome.dashboard.views.explore_models_view import explore_models_view
+from crome.dashboard.views.detect_incidents_view import detect_incidents_view
 
 __all__ = [
     'explore_dataset_view',
