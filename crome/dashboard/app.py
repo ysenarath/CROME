c65688005ce0110a710b264b714d53ca5ae80bbe
Yasas <ysenarath.93@gmail.com>
2022-07-31 10:35:01 -0400

    Update app icon

--- crome/dashboard/app.py
+++ crome/dashboard/app.py
@@ -2,7 +2,13 @@ import streamlit as st
 
 from crome.dashboard.views import explore_dataset_view, explore_models_view, detect_incidents_view
 
-st.set_page_config(page_title='CROMEx', layout='wide', initial_sidebar_state='auto', menu_items=None)
+st.set_page_config(
+    page_title='⏳ CROMEx',
+    page_icon='⏳',
+    layout='wide',
+    initial_sidebar_state='auto',
+    menu_items=None
+)
 
 page_names_to_funcs = {
     'Explore Dataset': explore_dataset_view,
